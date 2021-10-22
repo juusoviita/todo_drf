7de24253d691671c382f496fc44627647a598c5e
juusoviita <juusoviita@gmail.com>
2021-10-22 11:59:38 +0300

    minor updates to frontend

--- frontend/src/components/Task.js
+++ frontend/src/components/Task.js
@@ -1,19 +1,10 @@
-import { useDrag } from 'react-dnd'
 import { FaTimes } from 'react-icons/fa'
-import { ItemTypes } from './Items'
 import Button from "./Button"
  
 const Task = ({task, onDelete, onToggle, onChange, onEdit}) => {
 
-  const [{ isDragging }, drag] = useDrag({
-    type: ItemTypes.CARD,
-    collect: monitor => ({
-      isDragging: !!monitor.isDragging(),
-    })
-  })
-
   return (
-    <div ref={drag} className={`task-wrapper flex-wrapper ${task.reminder && 'task-reminder' } ${task.completed && 'task-completed' }`} onDoubleClick={() => onToggle(task.id)}>
+    <div className={`task-wrapper flex-wrapper ${task.reminder && 'task-reminder' } ${task.completed && 'task-completed' }`} onDoubleClick={() => onToggle(task.id)}>
       <div className={`${task.completed && 'strike-through'}`} style={{flex: 7, textAlign: 'left'}}>{ task.title }</div>
       <div style={{flex: 3, textAlign: 'center'}}>{ task.due_date }</div>
       <div style={{flex: 1}} className="task-checkbox">
--- frontend/src/components/TaskList.js
+++ frontend/src/components/TaskList.js
@@ -1,18 +1,9 @@
 import Task from "./Task"
-import { useDrop } from 'react-dnd'
-import { ItemTypes } from "./Items"
 
 const TaskList = ({ tasks, onDelete, onToggle, onChange, onEdit }) => {
-  
-  const[{ isOver }, drop] = useDrop({
-    accept: ItemTypes.CARD,
-    collect: monitor => ({
-        isOver: !!monitor.isOver(),
-    })
-  })
 
   return (
-      <div ref={drop}>
+      <div>
         {tasks.map((task) => (
           <Task key={task.id} task={task} onDelete={onDelete} onToggle={onToggle} onChange={onChange} onEdit={onEdit} />
         ))}
